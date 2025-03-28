11f92bd0df80ef750707af6cb560ee9ad07584bb
sohv <126096232+sohv@users.noreply.github.com>
2025-03-29 00:18:52 +0530

    removed unnecessary files

--- OCR/easy_ocr.py
+++
@@ -1,9 +0,0 @@
-import easyocr
-
-def extract_text_from_image(img_path, lang_list=['en']):
-    reader = easyocr.Reader(lang_list)
-    result = reader.readtext(img_path)
-    extracted_text = " ".join([text[1] for text in result])
-    return extracted_text
-
-
--- OCR/parse.py
+++ OCR/parse.py
@@ -1,7 +1,6 @@
 from docling.document_converter import DocumentConverter
 
-source = "images/college id.pdf"  # document per local path or URL
+source = "images/college id.pdf"
 converter = DocumentConverter()
 result = converter.convert(source)
 print(result.document.export_to_markdown())
-# output: ## Docling Technical Report [...]"
